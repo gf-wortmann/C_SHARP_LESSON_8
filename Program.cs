c65f13356e933d355933599c1cf39623e94e5f76
George Wortmann <gf.wortmann@gmail.com>
2022-12-02 16:54:58 +0300

    task 60 committed 1

--- Program.cs
+++ Program.cs
@@ -1,67 +1,47 @@
-﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 //Массив размером 2 x 2 x 2
 //66(0,0,0) 25(0,1,0)
 //34(1,0,0) 41(1,1,0)
 //27(0,0,1) 90(0,1,1)
 //26(1,0,1) 55(1,1,1)
 
-//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-//Например, даны 2 матрицы:
-//2 4 | 3 4
-//3 2 | 3 3
-//Результирующая матрица будет:
-//18 20
-//15 18
 
 
-int [,] MultiplyMatrices (int [,] A, int [,] B)
+int [,,] FillArray (int layers, int rows, int columns, int min, int max) 
 {
-    int rowsInA = A.GetUpperBound(0)+1;
-    int columnsInA = A.Length / rowsInA;
-    int rowsInB = B.GetUpperBound(0)+1;
-    int columnsInB = B.Length / rowsInB;
-    int [,] result = new int [rowsInA, columnsInB];
-    if (columnsInA != rowsInB) return (FillArray (rowsInA, columnsInB, 0, 0));
-    
-    for (int i = 0; i < rowsInA; i++ )
+    int [,,] result = new int [layers, rows, columns];
+    Random r = new Random();
+    for (int i = 0; i < layers; i++)
     {
-        for (int j = 0; j < columnsInB; j++)
+        for (int j = 0; j < rows; j++)        
         {
-            for (int r = 0; r < columnsInA; r++)
+            for (int k = 0; k < columns; k++)
             {
-                result [i, j] += (A [i,r] * B[r, j]);
+                result [i, j, k] = r.Next(min+1, max);
             }
         }
-
     }
     return result;
 }
 
-int [,] FillArray (int rows, int columns, int min, int max) 
-{
-    int [,] result = new int [rows, columns];
-    Random k = new Random();
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-           result [i,j] = k.Next(min+1, max);
-        }
-    }
-    return result;
-}
-
-void WriteArray2D ( int [,] Array)
+void PrintArray3D ( int [,,] Array)
 {
     int rows = Array.GetUpperBound(0)+1;
-    int columns = Array.Length / rows;
+    int columns = Array.GetUpperBound(1) + 1;
+    int layers = Array.GetUpperBound(2) + 1;
     
-    for (int i = 0; i < rows; i++)
+    for (int k = 0; k < layers; k++)
     {
         for (int j = 0; j < columns; j++)
-        {
-            string c = (Array[i,j] > 0) ? " " : "";
-            Console.Write($"\t{c}{Array [i, j]}");
+        {   
+            for (int i = 0; i < rows; i++)
+            {
+                string c = (Array[i, j, k] > 0) ? " " : "";
+                Console.Write($"\t{c}{Array [i, j, k]} ");
+                Console.Write($"[{i}, {j}, {k}]");
+            }
+            Console.Write("\n");
         }
         Console.Write("\n");
     }
@@ -69,29 +49,7 @@ void WriteArray2D ( int [,] Array)
 
 //=======================================
 
-int cols, rows;
-
 Console.Clear();
-Console.WriteLine("Enter number of rows in the 1st matrix: ");
-rows = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Enter number of columns in the 1st matrix: ");
-cols = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"\nIt is assumed 2nd matrix has {cols} rows and {rows} columns.\n");
-
-int [,] matrixA = FillArray (rows, cols, -10, 10);
-//int [,] matrixA = {{2, 4}, {3, 2}};
-Console.WriteLine($"The Matrix A is: ");
-WriteArray2D(matrixA);
-
-int [,] matrixB = FillArray (cols, rows, -10, 10);
-//int [,] matrixB = {{3, 4}, {3, 3}};
-Console.WriteLine($"The Matrix B is: ");
-WriteArray2D(matrixB);
-
-int [,] Product;// = new int [rows, rows];
-//int [,] Product = new int [2, 2]; //to be clarified
-Product = MultiplyMatrices (matrixA, matrixB);
-Console.WriteLine($"The Product Matrix is: ");
-WriteArray2D (Product);
-Console.WriteLine("\n\n");
+int [,,] matrix3 = FillArray(2,2,2,0,10);
+PrintArray3D(matrix3);
